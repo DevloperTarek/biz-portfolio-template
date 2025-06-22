c2d0ac9dc6e1cd7fabc5e747c24ec79f33762297
Tarek Hossain <devlopertarek433@gmail.com>
2025-06-22 12:55:49 +0600

    Note:biz team member area responsive issue solve

--- js/custom-text-rounded.js
+++ js/custom-text-rounded.js
@@ -1,13 +1,13 @@
-// Cutom Text 360deg Round
-const text = "mynameistarekhossain";
-const circle = document.getElementById("circle-text");
-const radius = 92; // Radius of the circle
-const deg = 360 / text.length;
+// // Cutom Text 360deg Round
+// const text = "mynameistarekhossain";
+// const circle = document.getElementById("circle-text");
+// const radius = 92; // Radius of the circle
+// const deg = 360 / text.length;
 
-for (let i = 0; i < text.length; i++) {
-    const char = document.createElement("span");
-    char.innerText = text[i];
-    const angle = deg * i;
-    char.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(${angle * -1}deg)`;
-    circle.appendChild(char);
-}
+// for (let i = 0; i < text.length; i++) {
+//     const char = document.createElement("span");
+//     char.innerText = text[i];
+//     const angle = deg * i;
+//     char.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(${angle * -1}deg)`;
+//     circle.appendChild(char);
+// }
